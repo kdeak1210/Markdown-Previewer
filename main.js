4f08d90c4e58681ed4d9669f6e0dd381ac1323ea
kyle <kdeak1210@gmail.com>
2017-08-31 19:15:40 -0400

    Handling for Headings 1-6
    Settled on a parsing method for the input string. Decided to split string into an array delimited by newline characters - each 'chunk' will be processed by a switch statement, then handled further. Completed the handling for headings 1-6 (h1, h2, h3...). So typing markdown for headings like ####Heading 4 will now work. syntax-helpers.js will hold the methods for handling specific markdown syntax groups

--- main.js
+++ main.js
@@ -2,14 +2,14 @@
 const inputTA = document.querySelector('#textEntry');
 const outputDiv = document.querySelector('#output');
 
-// Once typingTimer reaches the interval, the text processing function fires
+// On timer reaches interval, text processing function fires on textarea content
 const finishedTypingInterval = 1000;
 let typingTimer;
 
 // keyup event listener begins the timer
 inputTA.addEventListener('keyup', function() {
   clearTimeout(typingTimer);
-  typingTimer = setTimeout(process, finishedTypingInterval);
+  typingTimer = setTimeout(() => process(inputTA.value), finishedTypingInterval);
 }, false);
 
 // keydown event listener resets the timer
@@ -17,6 +17,22 @@ inputTA.addEventListener('keydown', function() {
   clearTimeout(typingTimer);
 }, false);
 
-function process() {
-  outputDiv.innerHTML = inputTA.value;
+function process(text) {
+  // Split content of textarea into string array delimited by newline characters
+  let textChunks = text.split('\n');
+  let myHTML = '';
+  console.log(textChunks);
+
+  textChunks.forEach(function(chunk) {
+    let firstChar = chunk.charAt(0);
+    let length = chunk.length;
+    console.log(length);
+
+    switch(firstChar) {
+      case '#':
+        myHTML += handleHeadings(chunk, length);
+        break;
+    }
+  });
+  outputDiv.innerHTML = myHTML;
 }
---
+++ syntax-helpers.js
@@ -0,0 +1,9 @@
+// Returns HTML for a segment of markdown headings
+function handleHeadings(text, length) {
+  return text.charAt(1) !== '#' ? `<h1>${text.substring(1, length)}</h1>` :
+         text.charAt(2) !== '#' ? `<h2>${text.substring(2, length)}</h2>` :
+         text.charAt(3) !== '#' ? `<h3>${text.substring(3, length)}</h3>` :
+         text.charAt(4) !== '#' ? `<h4>${text.substring(4, length)}</h4>` :
+         text.charAt(5) !== '#' ? `<h5>${text.substring(5, length)}</h5>` :
+         `<h6>${text.substring(6, length)}</h6>`
+}
